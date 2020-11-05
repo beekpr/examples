ca73b42f6d202853f2eccdce4ffcec999a1063ce
Florian Dreschner <code@dreschner.eu>
2020-11-05 08:57:51 +0100

    FUL-23392: Add documentation to JWT payload

--- home-screen-data-source/src/types.d.ts
+++ home-screen-data-source/src/types.d.ts
@@ -6,17 +6,26 @@ declare namespace Express {
 
 
 interface JWTPayload {
+    /** Version number of JWT */
     beekeeper_version: string;
     beekeeper_user: {
         name: string;
         avatar: string;
     }
+    /** Issuer */
     iss: string;
+    /** Fully Qualified Domain Name of Tenant */
     beekeeper_fqdn: string;
+    /** JWT Token Identifier */
     jti: string;
+    /** Expiration Date */
     exp: number;
+    /** Issued At */
     iat: number;
+    /** Don't use before */
     nbf: number;
+    /** Tenant User Id */
     tenantuserid: string | null;
+    /** Subject, in this case user id */
     sub: string;
 }
